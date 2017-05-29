3a42380e8228e45da61d043984d0c05495961e8a
Quentin Monmert <q.monmert@proxiad.com>
2017-05-29 14:57:42 +0200

    Install and use styled-components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: palevioletred;
+`;
+
+const Button = styled.button`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  color: white;
+`;
+
+const TomatoButton = styled(Button)`
+  background: tomato;
+`;
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Title>Title</Title>
+        <TomatoButton>TomatoButton</TomatoButton>
       </div>
     );
   }
+
 }
 
 export default App;
